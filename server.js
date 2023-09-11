dda1d4a46cd1d9c6b5dba01d78d2365e83ddffdd
MedalMan <71987699+MedalMan@users.noreply.github.com>
2023-09-10 20:09:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-Error checking https://enterprise.lmaero.us.lmco.com/qmd/dspSearchResults.cfm: module 'botocore.vendored.requests' has no attribute 'get'
+import json
+from botocore.vendored import requests
+
+def lambda_handler(event, context):
+    urls = ["https://enterprise.lmaero.us.lmco.com/qmd/dspSearchResults.cfm"]
+    print(urls)
+    for url in urls:
+        try:
+            response = requests.api.request("GET", url)
+            print(response)
+            if response.status_code != 200:
+                message = f"Site {url} returned {response.status_code} status code"
+                print('ok')
+                print(message)
+        except Exception as e:
+            message = f"Error checking {url}: {str(e)}"
+            print(message)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Monitoring completed.')
+    }
