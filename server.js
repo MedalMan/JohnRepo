58774fc7a1972d2ce5240fa59048dc73d68f8e81
MedalMan <71987699+MedalMan@users.noreply.github.com>
2023-09-12 14:28:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import json
-from botocore.vendored import requests
+import requests
 
 def lambda_handler(event, context):
-    urls = ["https://enterprise.lmaero.us.lmco.com/qmd/dspSearchResults.cfm"]
+    urls = ["https://sit-iiq.us.lmco.com"]
     print(urls)
     for url in urls:
         try:
-            response = requests.api.request("GET", url)
+            response = requests.request("GET", url)
             print(response)
             if response.status_code != 200:
                 message = f"Site {url} returned {response.status_code} status code"
