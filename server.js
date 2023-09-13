54148f54221bae044e331ca70fd14f03f4147ced
MedalMan <71987699+MedalMan@users.noreply.github.com>
2023-09-13 15:50:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-import json
-import requests
-
-def lambda_handler(event, context):
-    urls = ["https://sit-iiq.us.lmco.com"]
-    print(urls)
-    for url in urls:
-        try:
-            response = requests.request("GET", url)
-            response.raise_for_status()  # Raise an exception for HTTP errors (non-200 status codes)
-            print(response)
-        except requests.exceptions.RequestException as e:
-            message = f"Error checking {url}: {str(e)}"
-            print(message)
-        except Exception as e:
-            message = f"Unexpected error for {url}: {str(e)}"
-            print(message)
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Monitoring completed.')
-    }
+{
+  "errorType": "ReferenceError",
+  "errorMessage": "exports is not defined in ES module scope",
+  "trace": [
+    "ReferenceError: exports is not defined in ES module scope",
+    "    at file:///var/task/index.mjs:3:1",
+    "    at ModuleJob.run (node:internal/modules/esm/module_job:194:25)"
+  ]
+}
